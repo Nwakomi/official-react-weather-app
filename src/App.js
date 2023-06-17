8f56423a6343eb4118d3692518eb1e0bf7e5080c
Nwakomi <natali.krmz@gmail.com>
2023-06-17 11:39:02 +0200

    added the github repo url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App"> Weather App</div>
+    <div className="App">
+      <h1>Weather App</h1>
+    <footer>
+This project was coded by Natali and is 
+    <a href="https://github.com/Nwakomi/official-react-weather-app" target="_blank"> open-sourced on Github</a>
+    </footer>
+    </div>
   );
 }
 
