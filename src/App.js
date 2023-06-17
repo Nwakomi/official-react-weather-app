4c73af0aca934b0de14cb18470098b40c30ba866
Nwakomi <natali.krmz@gmail.com>
2023-06-17 21:19:12 +0200

    added styling in css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
+import Weather from './Weather';
 import './App.css';
+import React from 'react';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Weather App</h1>
+    
+      <Weather />
     <footer>
 This project was coded by Natali and is 
     <a href="https://github.com/Nwakomi/official-react-weather-app" rel="noreferrer" target="_blank"> open-sourced on Github</a>
