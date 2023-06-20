1ad16e6b2e619143c7e9303c0bfd7f3205c80a33
Nwakomi <natali.krmz@gmail.com>
2023-06-20 13:02:21 +0200

    removed fahrenheit, added useEffect

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,4 @@
-import React, {useState} from "react";
-
+import React, {useState, useEffect} from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -8,19 +7,32 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props){
     let [loaded, setLoaded] = useState (false);
     let [forecast, setForecast] = useState (false);
+
+useEffect (() => {
+    setLoaded (false);
+}, [props.coordinates]);
+
     function handleResponse(response){
-    console.log(response.data);
-    setForecast(response.data.daily);
-    setLoaded(true)
+        setForecast(response.data.daily);
+        setLoaded(true)
 ;}
 
 if (loaded) {
 return(
     <div className="WeatherForecast">
     <div className="row">
-      <div className="col"> 
-      <WeatherForecastDay data={forecast[0]}/> 
-    </div>
+        {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+           
+                return (
+                    <div className="col" key={index}>
+                <WeatherForecastDay data={dailyForecast}/> 
+                </div>
+                );
+            } else {
+                return null;
+            }
+                })}
     </div>
     </div>
     );
